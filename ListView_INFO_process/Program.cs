54e9e03a27ff691d83e0b4b7a1043b5d3d045a71
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-22 17:16:12 +0200

    Add project files.

---
+++ ListView_INFO_process/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ListView_INFO_process
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.GetProcesses().ToList().ForEach(p => Console.WriteLine($"{p.Id}. {p.ProcessName}"));
+            Console.ReadLine();
+        }
+    }
+}
